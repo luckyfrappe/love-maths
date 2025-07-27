0ef427d9ff97cec390a14b8d15c79653c849c469
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2025-07-27 07:50:42 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,6 +68,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiplication"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtraction"];
     } else {
         alert(`Unknown operator: ${operator}`);
         throw `Unknown operator: ${operator}. Aborting!`;
@@ -98,8 +100,10 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractionQuestion() {
-
+function displaySubtractionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 }
 
 function displayMultiplicationQuestion(operand1, operand2) {
